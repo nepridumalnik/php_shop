a5f5cf0613023477df4609052c79d97dc069e6bc
Dudnik Pavel <ne.pridumal.nik@yandex.ru>
2023-05-24 17:54:47 +0700

    Добавлено имя пользователя в БД

--- config/config_db.php
+++ config/config_db.php
@@ -1,9 +1,9 @@
 <?php
 
 define('DB_CONFIG', [
-    'dsn' => 'mysql:host=mysql;port=3306;dbname=ishop;charset=utf8',
-    'user' => 'root',
-    'pass' => getenv('MYSQL_ROOT_PASSWORD'),
+    'dsn' => 'mysql:host=mysql;port=3306;dbname=' . getenv('DB_NAME') . ';charset=utf8',
+    'user' => getenv('DB_USER'),
+    'pass' => getenv('DB_PASSWORD'),
 ]);
 
 return DB_CONFIG;
